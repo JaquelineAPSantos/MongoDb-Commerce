99e0c195df395ef707b6c3d435dc97d1906126c5
Jaqueline Santos <jaque.aps@hotmail.com>
2022-11-09 16:43:57 -0300

    feat: Criado arquivo desafio13 e implementada a query

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,7 +1,4 @@
 b.produtos.find({
     nome: { $nin: ["Big Mac", "McChicken"] } },
-    { nome: 1, 
-      curtidas: 1, 
-      vendidos: 1, 
-      _id: 0 })
+    { nome: 1, curtidas: 1, vendidos: 1, _id: 0 })
     .pretty();
